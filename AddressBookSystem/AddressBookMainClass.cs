a488fff026488cb0acfb6f25366dc96d7fa59318
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-31 15:12:07 +0530

    [Madhavee] Add Deleting Record Feature

--- AddressBookSystem/AddressBookMainClass.cs
+++ AddressBookSystem/AddressBookMainClass.cs
@@ -16,6 +16,7 @@ namespace AddressBookSystem
                 Console.WriteLine("\n\nWelcome to Address Book System");               
                 Console.WriteLine("1. Add a new Record");
                 Console.WriteLine("2. Update a Record");
+                Console.WriteLine("3. Delete a Record");
                 Console.WriteLine("4.Exit");
                 Console.WriteLine("\nEnter your choice : ");
                 
@@ -27,11 +28,17 @@ namespace AddressBookSystem
                         records.PrintRecords();
                         break;
                     case 2:
-                        Console.WriteLine("Enter your First Name : ");
+                        Console.WriteLine("\nEnter your First Name : ");
                         string fn = Console.ReadLine();
                         Console.WriteLine("Enter your Last Name : ");
                         string ln = Console.ReadLine();
                         records.UpdateRecords(fn, ln);
+                        records.PrintRecords();
+                        break;
+                    case 3:
+                        Console.WriteLine("\nEnter your first name which you want to delete : ");
+                        string f = Console.ReadLine();
+                        records.DeleteRecord(f);
                         records.PrintRecords();
                         break;
                     case 4:
