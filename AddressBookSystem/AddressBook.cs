e2fc48287e6959557d0027d21b543b488b77a7ac
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-01 11:54:42 +0530

    Update AddressBook.cs

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -8,13 +8,11 @@ namespace AddressBookSystem
 {
     internal class AddressBook
     {
-        
-
-        List<PersonInput> addressbook = new List<PersonInput>();
-        public void AddRecords()
+        List<PersonInput> addressbook = new List<PersonInput>(); // Creating List having PersonInput Class Object Datatype
+        public void AddRecords() // Creating class method to add Person Record in List
         {
-            PersonInput input = new PersonInput();
-            Details:            
+            PersonInput input = new PersonInput(); // Creating a object of PersonInput Class
+            // Getting all the details from user and store it in PersonInput Class variales through object         
             Console.WriteLine("\nEnter your First Name : ");
             input.fName = Console.ReadLine();
             Console.WriteLine("Enter your Last Name : ");
@@ -31,35 +29,13 @@ namespace AddressBookSystem
             input.phoneNumber = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter your Email Address: ");
             input.email = Console.ReadLine();
-
-            if(addressbook.Count==0)
-            {
-                addressbook.Add(input);
-                Console.WriteLine("\nRecord Added successfully in Address Book");
-            }
-            else
-            { 
-                foreach(var record in addressbook)
-                {
-                    if (record.fName == input.fName)
-                    {
-                        Console.WriteLine("\nThis record is already present in AddressBook");
-                        goto Details;
-                    }
-                    else
-                    {
-                        addressbook.Add(input);
-                        Console.WriteLine("\nRecord Added successfully in Address Book");
-                    }
-                }
-            }
-                                
-        }
-        int i = 1;
-        public void PrintRecords()
-        {
-            
-            foreach (var record in addressbook)
+            addressbook.Add(input); // Adding all the details of user as a one record in List
+            Console.WriteLine("Record Added successfully in Address Book");                              
+        }        
+        public void PrintRecords() // Creating a class method to just display all the records of list
+        {     
+            int i = 1;
+            foreach (var record in addressbook) // Accessing all the records of list one by one using foreach loop
             {
                 Console.WriteLine("\n\nRecord - "+i);
                 Console.WriteLine("First Name : "+record.fName);
@@ -69,83 +45,75 @@ namespace AddressBookSystem
                 Console.WriteLine("State : "+record.state);
                 Console.WriteLine("Email : "+record.email);
                 Console.WriteLine("Zip code : "+record.zip);
-                Console.WriteLine("Phone Number : "+record.phoneNumber);
-                
+                Console.WriteLine("Phone Number : "+record.phoneNumber);              
                 i++;
             }
         }
-
-        public void UpdateRecords(string fn,string ln)
+        public void UpdateRecords(string fn,string ln) // Creating class method to update record which takes first name and last name as parameter
         {
-            foreach(var record in addressbook)
+            foreach(var record in addressbook) // Accessing all the records of list one by one using foreach loop
             {
-                if (record.fName == fn && record.lName == ln)
+                if (record.fName == fn && record.lName == ln) // Checking that first name and last name provided by user is matching with Existing Reord or not
                 {
-                    while (true)
+                   
+                    Console.WriteLine("\n\nWhich field you want to update : ");
+                    Console.WriteLine("\n1:First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Email\n7.Zip Code\n8.PhoneNumber\n9.Exit");
+                    Console.WriteLine("Enter your Choice : ");
+                    int ch = Convert.ToInt32(Console.ReadLine()); // Store the user choice which want to update 
+                    switch (ch) 
                     {
-                        Console.WriteLine("\n\nWhich field you want to update : ");
-                        Console.WriteLine("\n1:First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Email\n7.Zip Code\n8.PhoneNumber\n9.Exit");
-                        Console.WriteLine("Enter your Choice : ");
-                        int ch = Convert.ToInt32(Console.ReadLine());
-                        switch (ch)
-                        {
-                            case 1:
-                                Console.WriteLine("\nEnter new first name : ");
-                                string f = Console.ReadLine();
-                                record.fName = f;
-                                Console.WriteLine("\nFirst Name Updated Successfully");
-                                break;
-                            case 2:
-                                Console.WriteLine("\nEnter new last name : ");
-                                string l = Console.ReadLine();
-                                record.lName = l;
-                                Console.WriteLine("\nLast Name Updated Successfully");
-                                break;
-                            case 3:
-                                Console.WriteLine("\nEnter new address : ");
-                                string a = Console.ReadLine();
-                                record.address = a;
-                                Console.WriteLine("\nAddress Updated Successfully");
-                                break;
-                            case 4:
-                                Console.WriteLine("\nEnter new city name : ");
-                                string c = Console.ReadLine();
-                                record.city = c;
-                                Console.WriteLine("\nCity Name Updated Successfully");
-                                break;
-                            case 5:
-                                Console.WriteLine("\nEnter new state : ");
-                                string s = Console.ReadLine();
-                                record.state = s;
-                                Console.WriteLine("\nState Name Updated Successfully");
-                                break;
-                            case 6:
-                                Console.WriteLine("\nEnter new email : ");
-                                string e = Console.ReadLine();
-                                record.email = e;
-                                Console.WriteLine("\nEmail Updated Successfully");
-                                break;
-                            case 7:
-                                Console.WriteLine("\nEnter new Zip Code : ");
-                                int z = Convert.ToInt32(Console.ReadLine());
-                                record.zip = z;
-                                Console.WriteLine("\nZip Code Updated Successfully");
-                                break;
-                            case 8:
-                                Console.WriteLine("\nEnter new Phone Number : ");
-                                int p = Convert.ToInt32(Console.ReadLine());
-                                record.phoneNumber = p;
-                                Console.WriteLine("\nPhone Number Updated Successfully");
-                                break;
-                            case 9:
-                                System.Environment.Exit(0);
-                                break;
-                            default:
-                                Console.WriteLine("Enter valid choice");
-                                break;
-                        }
-                    }
-
+                        case 1:
+                            Console.WriteLine("\nEnter new first name : ");
+                            string f = Console.ReadLine();
+                            record.fName = f; // Update the first name of record in address book
+                            Console.WriteLine("\nFirst Name Updated Successfully");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nEnter new last name : ");
+                            string l = Console.ReadLine();
+                            record.lName = l; // Update the last name of record in address book
+                            Console.WriteLine("\nLast Name Updated Successfully");
+                            break;
+                        case 3:
+                            Console.WriteLine("\nEnter new address : ");
+                            string a = Console.ReadLine();
+                            record.address = a;// Update the address of record in address book
+                            Console.WriteLine("\nAddress Updated Successfully");
+                            break;
+                        case 4:
+                            Console.WriteLine("\nEnter new city name : ");
+                            string c = Console.ReadLine();
+                            record.city = c; // Update the city name of record in address book
+                            Console.WriteLine("\nCity Name Updated Successfully");
+                            break;
+                        case 5:
+                            Console.WriteLine("\nEnter new state : ");
+                            string s = Console.ReadLine();
+                            record.state = s; // Update the state name of record in address book
+                            Console.WriteLine("\nState Name Updated Successfully");
+                            break;
+                        case 6:
+                            Console.WriteLine("\nEnter new email : ");
+                            string e = Console.ReadLine();
+                            record.email = e; // Update the email name of record in address book
+                            Console.WriteLine("\nEmail Updated Successfully");
+                            break;
+                        case 7:
+                            Console.WriteLine("\nEnter new Zip Code : ");
+                            int z = Convert.ToInt32(Console.ReadLine());
+                            record.zip = z; // Update the zipcode of record in address book
+                            Console.WriteLine("\nZip Code Updated Successfully");
+                            break;
+                        case 8:
+                            Console.WriteLine("\nEnter new Phone Number : ");
+                            int p = Convert.ToInt32(Console.ReadLine());
+                            record.phoneNumber = p; // Update the phone number of record in address book
+                            Console.WriteLine("\nPhone Number Updated Successfully");
+                            break;
+                        default:
+                            Console.WriteLine("Enter valid choice");
+                            break;
+                    }                   
                 }
                 else
                 {
