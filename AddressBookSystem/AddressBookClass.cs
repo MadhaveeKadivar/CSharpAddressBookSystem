864134e60bd6d680f1eac5fb9c23ff38ea751842
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-01 11:18:52 +0530

    Update AddressBookClass.cs

--- AddressBookSystem/AddressBookClass.cs
+++ AddressBookSystem/AddressBookClass.cs
@@ -11,8 +11,8 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System");
-            ContactsRecord record = new ContactsRecord();
-            record.Input();
+            ContactsRecord record = new ContactsRecord(); // Creating object of ContactRecord class
+            record.Input(); // Calling a Input Method of ContactRecord Class
         }
     }
 }
