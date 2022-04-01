aeaf2828d5a3564b28260dcad2f579c3306f8d20
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-01 11:21:58 +0530

    Update ContactsRecord.cs

--- AddressBookSystem/ContactsRecord.cs
+++ AddressBookSystem/ContactsRecord.cs
@@ -8,26 +8,27 @@ namespace AddressBookSystem
 {
     internal class ContactsRecord
     {
-        public void Input()
+        public void Input() //Creating class method
         {
+            //Declaring Variable
             string fName,lName,address,city,state ,email ;
             int zip, phoneNumber;
             Console.WriteLine("Enter your First Name : ");
-            fName = Console.ReadLine();
+            fName = Console.ReadLine(); //Read firstname as input which is provided by user
             Console.WriteLine("Enter your Last Name : ");
-            lName = Console.ReadLine();
+            lName = Console.ReadLine(); //Read lastname as input which is provided by user
             Console.WriteLine("Enter your Address : ");
-            address = Console.ReadLine();
+            address = Console.ReadLine(); //Read address as input which is provided by user
             Console.WriteLine("Enter your City Name : ");
-            city = Console.ReadLine();
+            city = Console.ReadLine(); //Read city name as input which is provided by user
             Console.WriteLine("Enter your State Name : ");
-            state = Console.ReadLine();
+            state = Console.ReadLine(); //Read state name as input which is provided by user
             Console.WriteLine("Enter your Zip Code : ");
-            zip = Convert.ToInt32(Console.ReadLine());
+            zip = Convert.ToInt32(Console.ReadLine()); //Read zip code as input which is provided by user
             Console.WriteLine("Enter your Phone Number : ");
-            phoneNumber = Convert.ToInt32(Console.ReadLine());
+            phoneNumber = Convert.ToInt32(Console.ReadLine()); //Read phone number as input which is provided by user
             Console.WriteLine("Enter your Email Address: ");
-            email = Console.ReadLine();
+            email = Console.ReadLine(); //Read email as input which is provided by user
         }
     }
 }
