35075a739b44551ceb6c42cac24d01f5087b7e56
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-01 11:38:41 +0530

    Update AddressBookMainClass.cs

--- AddressBookSystem/AddressBookMainClass.cs
+++ AddressBookSystem/AddressBookMainClass.cs
@@ -11,20 +11,18 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book System");
-            AddressBook records = new AddressBook();
+            AddressBook records = new AddressBook(); // Creating a object of AddressBook class
             Console.WriteLine("1. Add a new Record");
             Console.WriteLine("Enter your choice : ");
-            int ch = Convert.ToInt32(Console.ReadLine());
-            switch(ch)
+            int ch = Convert.ToInt32(Console.ReadLine()); // Storing a user choice in variable
+            switch(ch) // 
             {
                 case 1:
-                    records.AddRecords();
-                    records.PrintRecords();
+                    records.AddRecords(); // Calling a method of AddressBook class to add record in address book
+                    records.PrintRecords(); // Calling a method of AddressBook class to display records of address book
                     Console.ReadLine();
                     break;
-            }
-            
-            
+            }          
         }
     }
 }
